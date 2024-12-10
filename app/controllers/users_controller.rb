a5819b32c7e06ff3ffa31be261cb6fafc894511e
LovarinJP <187247562+LovarinJP@users.noreply.github.com>
2024-12-10 06:53:20 +0000

    ユーザー画面修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
+  end
+
+  def index
+    @users = User.all
   end
   
   def edit
@@ -23,6 +29,13 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
   end
 
 end
